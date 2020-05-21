25b53c37e35a3b588a8243481c831dea7be9777c
Jacqueline Lee <jbilston@atb.com>
2020-05-20 20:31:06 -0600

    Remove useless semi-colon

--- src/test/groovy/LiftSpecification.groovy
+++ src/test/groovy/LiftSpecification.groovy
@@ -3,7 +3,7 @@ import spock.lang.Specification
 class LiftSystemSpecification extends Specification{
     def "Lift is stationary if no buttons are pressed"() {
         given:
-            def interiorButtonPanel = new InteriorButtonPanel();
+            def interiorButtonPanel = new InteriorButtonPanel()
             def lift = new Lift(0, interiorButtonPanel)
         expect:
             lift.direction == Lift.Directions.STATIONARY
